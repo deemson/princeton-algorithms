ad44b5b75c5a9098824fa7383ccd278a40d1a7c3
Dmytro Kurkin <deemson.k@gmail.com>
2024-08-01 11:17:24 +0300

    added test

--- part1/symboltable/s0_symbol_table_test.go
+++ part1/symboltable/s0_symbol_table_test.go
@@ -1,20 +1,34 @@
-package symboltable
+package symboltable_test
 
 import (
 	"github.com/deemson/princeton-algorithms/lib/compare"
+	"github.com/deemson/princeton-algorithms/part1/symboltable"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 type allAlgorithmsParams[K, V any] struct {
+	equal compare.Func[K]
 }
 
-func (p allAlgorithmsParams[K, V]) do(f func(t *testing.T, table SymbolTable[K, V])) {
-
+func (p allAlgorithmsParams[K, V]) do(t *testing.T, f func(t *testing.T, symbolTable symboltable.SymbolTable[K, V])) {
+	namedSymbolTables := []struct {
+		name        string
+		symbolTable symboltable.SymbolTable[K, V]
+	}{
+		{"UnorderedLinkedList", symboltable.UnorderedLinkedList[K, V](p.equal)},
+	}
+	for _, namedSymbolTable := range namedSymbolTables {
+		t.Run(namedSymbolTable.name, func(t *testing.T) {
+			f(t, namedSymbolTable.symbolTable)
+		})
+	}
 }
 
 func TestSymbolTable(t *testing.T) {
-	allAlgorithmsParams[string, int]{}.do(func(t *testing.T, symbolTable SymbolTable[string, int]) {
+	allAlgorithmsParams[string, int]{
+		equal: compare.ComparableEqual[string],
+	}.do(t, func(t *testing.T, symbolTable symboltable.SymbolTable[string, int]) {
 		symbolTable.Set("one", 1)
 		symbolTable.Set("two", 2)
 		symbolTable.Set("three", 3)
