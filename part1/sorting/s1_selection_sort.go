28cb5eb49d06d288775c3c6979d11ae7e9955a9e
Dmytro Kurkin <deemson.k@gmail.com>
2024-08-01 22:22:28 +0300

    typo

--- part1/sorting/s1_selection_sort.go
+++ part1/sorting/s1_selection_sort.go
@@ -11,14 +11,14 @@ func SelectionSort[T any](array collection.SizedIndexedMutable[T], less compare.
 	for outerLoopIndex := 0; outerLoopIndex < array.Size(); outerLoopIndex++ {
 		// Each iteration of the outer loop it tries to find the minimum item index to the right
 		// of the current index and swap this item with the current item.
-		mininumItemIndex := outerLoopIndex
+		minimumItemIndex := outerLoopIndex
 		// Inner loop tries to find the minimum item to the right of the current one.
 		// This is why it starts at the current index of the outer loop.
 		for innerLoopIndex := outerLoopIndex; innerLoopIndex < array.Size(); innerLoopIndex++ {
-			if less(array.Get(innerLoopIndex), array.Get(mininumItemIndex)) {
-				mininumItemIndex = innerLoopIndex
+			if less(array.Get(innerLoopIndex), array.Get(minimumItemIndex)) {
+				minimumItemIndex = innerLoopIndex
 			}
 		}
-		collection.SwapIndexes[T](array, outerLoopIndex, mininumItemIndex)
+		collection.SwapIndexes[T](array, outerLoopIndex, minimumItemIndex)
 	}
 }
